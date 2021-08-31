20632bc2aa89bf548e10ead8bfb1030f9465fdb6
Almog Wertzberger <Almogish@gmail.com>
2021-08-31 10:27:11 +0300

    initial

---
+++ script.js
@@ -0,0 +1,40 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jess Bezos",
+  "Elon Musk",
+  "Bernard Arnault",
+  "Mark Zuckerberg",
+  "Bill Gates",
+  "Larry Page",
+  "Sergey Brin",
+  "Larry Ellison",
+  "Warren Buffett",
+  "Steve Ballmer",
+];
+
+//Store listitems
+const listItems = [];
+
+let dragStartIndex;
+
+//Insert list items into DOM
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+    listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+    <p class="person-name">${person}</p>
+    <i class="fas fa-grip-lines"></i>
+    </div>
+    `;
+    listItems.push(listItem);
+    draggable_list.appendChild(listItem);
+  });
+}
+
+createList();
